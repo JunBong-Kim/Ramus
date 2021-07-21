ba9370683e044ba1ab94bb57c19bebba0a207bd9
gktekr@naver.com <wnstlr5933!>
2021-07-21 12:47:28 +0900

    splash

---
+++ app/src/main/java/com/hackathon/ramus/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.hackathon.ramus;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.bumptech.glide.Glide;
+import com.hackathon.ramus.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+    private FragmentStateAdapter adapter;
+    private int pageNumber = 5;
+    Animation animation;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        context = this;
+
+        bindView();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Glide.with(context).load(R.drawable.gif).into(binding.splash);
+            }
+        }, 500);
+
+
+
+        Handler handler2 = new Handler();
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(context, EmailAuthLoginActivity.class));
+                finish();
+            }
+        }, 1800);
+
+
+
+    }
+
+
+    private void bindView() {
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+
+
+
+
+}
